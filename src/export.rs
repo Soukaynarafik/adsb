26ee4c7d395c66ab968f44122001195e89634604
Soukaynarafik <srafik3@myges.fr>
2026-05-17 15:48:54 +0200

    Update export.rs

--- src/export.rs
+++ src/export.rs
@@ -1,1 +1,15 @@
+use crate::aircraft::AircraftState;
+use std::error::Error;
+use std::fs::File;
 
+pub fn export_to_csv(filename: &str, data: &[AircraftState]) -> Result<(), Box<dyn Error>> {
+    let file = File::create(filename)?;
+    let mut wtr = csv::Writer::from_writer(file);
+
+    for record in data {
+        wtr.serialize(record)?;
+    }
+
+    wtr.flush()?;
+    Ok(())
+}
